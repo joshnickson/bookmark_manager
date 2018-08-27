21a484017fc7877466e8db0489f8407790b09b15
josh nickson <jnickson1506@gmail.com>
2018-08-27 17:57:02 +0100

    add bookmark model

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
 require 'sinatra/base'
+require_relative './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
+
   get '/' do
-    @bookmarks = [
-              'https://makersstudents.slack.com',
-              'https://www.bbc.co.uk/sport/football',
-              'https://makersstudents.slack.com',
-              ]
+    @bookmarks = Bookmark.all
+    # @bookmarks = [
+    #           'https://makersstudents.slack.com',
+    #           'https://www.bbc.co.uk/sport/football',
+    #           'https://makersstudents.slack.com',
+    #           ]
+    erb :index
   end
 
   run! if app_file == $0
---
+++ lib/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark
+  def self.all
+    [
+      'https://makersstudents.slack.com',
+      'https://www.bbc.co.uk/sport/football',
+      'https://makersstudents.slack.com',
+    ]
+  end
+end
